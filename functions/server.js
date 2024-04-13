265a706688b222101149c119fd842b866e7fde5a
wizard-ron17 <128203911+wizard-ron17@users.noreply.github.com>
2024-04-12 23:22:44 -0400

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 
 router.get('/api/account/userAccount', async (req, res) => {
     try {
-        const { account } = req.params;
+        const { userAccount } = req.params;
         // Call your API to fetch account data
         const response = await axios.get(`https://backend2.euclabs.net/kadena-indexer/v1/account/${userAccount}`);
         // Extract and send the response data back to the client
