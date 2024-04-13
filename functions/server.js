91da0365dc86b656b6745b766b2fe8a2dd2995da
wizard-ron17 <128203911+wizard-ron17@users.noreply.github.com>
2024-04-12 23:21:56 -0400

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -9,11 +9,11 @@ router.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-router.get('/api/account/k:account', async (req, res) => {
+router.get('/api/account/userAccount', async (req, res) => {
     try {
         const { account } = req.params;
         // Call your API to fetch account data
-        const response = await axios.get(`https://backend2.euclabs.net/kadena-indexer/v1/account/k:${account}`);
+        const response = await axios.get(`https://backend2.euclabs.net/kadena-indexer/v1/account/${userAccount}`);
         // Extract and send the response data back to the client
         res.json(response.data);
     } catch (error) {
