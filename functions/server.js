aaa70baea23c63daf0eb87cc96a53b6181c71cc4
wizard-ron17 <128203911+wizard-ron17@users.noreply.github.com>
2024-04-12 22:05:03 -0400

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
 const serverless = require('serverless-http');
+const axios = require('axios');
 
 const app = express();
+const router = express.Router();
 
-app.use(cors());
-
-// Root route handler
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-// API route handler
-app.get('/api/account/:account', async (req, res) => {
+router.get('/api/account/:account', async (req, res) => {
     try {
         const { account } = req.params;
+        // Call your API to fetch account data
         const response = await axios.get(`https://backend2.euclabs.net/kadena-indexer/v1/account/${account}`);
+        // Extract and send the response data back to the client
         res.json(response.data);
     } catch (error) {
         console.error('Error fetching API data:', error);
@@ -24,6 +22,7 @@ app.get('/api/account/:account', async (req, res) => {
     }
 });
 
-// Export your Express app as a Netlify Function handler
+app.use('/.netlify/functions/server/api', router);
+
 module.exports = app;
 module.exports.handler = serverless(app);
